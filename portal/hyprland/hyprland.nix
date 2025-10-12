9ff5d8a853d6bcec5df7a6dc6d9c6859f5f6ed05
Maduki-tech <d.schlueter1011@gmail.com>
2025-10-12 08:57:21 +0200

    New HyprConfig

--- portal/hyprland/hyprland.nix
+++ portal/hyprland/hyprland.nix
@@ -19,12 +19,13 @@
       };
 
       general = {
+        "$mainMod" = "SUPER";
         gaps_in = 5;
         gaps_out = 20;
         border_size = 2;
         # https://wiki.hypr.land/Configuring/Variables/#variable-types for info about colors
-        col.active_border = "rgba (33 ccffee) rgba (00 ff99ee) 45 deg";
-        col.inactive_border = "rgba (595959 aa)";
+        # col.active_border = "rgba (33 ccffee) rgba (00 ff99ee) 45 deg";
+        # col.inactive_border = "rgba (595959 aa)";
         # Set to true enable resizing windows by clicking and dragging on borders and gaps
         resize_on_border = false;
         # Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
